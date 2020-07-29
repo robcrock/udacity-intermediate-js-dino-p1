2f7543e1e54d373a3c77e8c41f635819a77c1cd4
robcrockPI <robert@propellerindustries.com>
2020-07-29 17:03:06 -0400

    Pushed comparison facts to the dino objects.

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 // Create Dino Constructor
 const Dino = function (obj) {
   return {
@@ -7,7 +8,7 @@ const Dino = function (obj) {
     diet: obj.diet,
     where: obj.where,
     when: obj.when,
-    fact: obj.fact,
+    fact: [],
   };
 };
 
@@ -22,7 +23,7 @@ const human = {
 };
 
 // Use IIFE to get human data from form
-const submit = function () {
+const Human = function () {
   const me = Object.create(human);
 
   me.name = document.getElementById('name').value;
@@ -31,80 +32,90 @@ const submit = function () {
   me.weight = document.getElementById('weight').value;
   me.diet = document.getElementById('diet').value;
 
+  return me;
   // const gridDiv = document.getElementById('grid');
   // const gridItem = document.createElement('div');
-  const gridItemHtml = `<div class="grid-item">
-      <img src="./images/human.png">
-      <h3>${me.name}</h3>
-  </div>`;
   // gridDiv.appendChild(gridItem).innerHTML = gridItemHtml;
   // console.log(me);
-  return gridItemHtml;
 };
 
 // Create Dino Compare Height
 // NOTE: Weight in JSON file is in lbs, height in inches.
-const compareHeight = function (hHeightFt, hHeightIn) {
-  const hTotalHeightInInches = hHeightFt / 12 + hHeightIn;
-  const dTotalHeightInInches = 10;
+const compareHeight = function (hHeightFt, hHeightIn, dTotalHeightInInches) {
+  const hTotalHeightInInches = parseInt(hHeightFt) * 12 + parseInt(hHeightIn);
 
-  if (hTotalHeightInInches > dTotalHeightInInches) {
-    console.log('This dino is shorter than the human.');
-  } else if (hTotalHeightInInches < dTotalHeightInInches) {
-    console.log('This dino is taller than the human.');
-  } else console.log('This dino is the same height as the human.');
+  if (hTotalHeightInInches > +dTotalHeightInInches) {
+    return 'This dino is shorter than the human.';
+  } if (hTotalHeightInInches < +dTotalHeightInInches) {
+    return 'This dino is taller than the human.';
+  } return 'This dino is the same height as the human.';
 };
 
 // Create Dino Compare Weight
 // NOTE: Weight in JSON file is in lbs, height in inches.
-const compareWeight = function (hWeight) {
-  const dWeight = hWeight; // REFACTOR
-  const weightDiff = dWeight - hWeight;
+const compareWeight = function (hWeight, dWeight) {
+  // const dWeight = hWeight; // REFACTOR
+  const weightDiff = parseInt(dWeight) - parseInt(hWeight);
 
   if (weightDiff > 0) {
-    console.log(`This dino is ${weightDiff}lbs heavier`);
-  } else if (weightDiff < 0) {
-    console.log(`This dino is ${weightDiff}lbs lighter`);
-  } else {
-    console.log('The dino and the human are the same weight.');
-  }
+    return `This dino is heavier than the human.`;
+  } if (weightDiff < 0) {
+    return `This dino is lighter than the human.`;
+  } return 'The dino and the human are the same weight.';
 };
 
 // Create Dino Compare Diet
 // NOTE: Weight in JSON file is in lbs, height in inches.
-const compareDiet = function (hDiet) {
-  const dDiet = 'Herbavor';
+const compareDiet = function (hDiet, dDiet) {
 
-  if (hDiet === dDiet) {
-    console.log('This dino and human have the same diet.');
-  } else {
-    console.log('This dino and human have different diets.');
-  }
+  if (hDiet.toLowerCase() === dDiet) {
+    return 'This dino and human have the same diet.';
+  } return 'This dino and human have different diets.';
 };
 
 // Generate Tiles for each Dino in Array
 const fetchDinoData = function () {
+  const human = Human();
+
+  const gridItemHuman =
+    `<div class="grid-item">
+      <img src="./images/human.png">
+      <h3>${human.name}</h3>
+  </div>`;
+
   fetch('dino.json')
     .then(response => response.json())
     .then(dinos => {
+
       // Create the array of grid items.
-      const gridItemsHtml = [];
+      const gridItemDinos = [];
+
+      // Push each div into the array
       dinos.Dinos.forEach(dino => {
-        gridItemsHtml.push(`<div class="grid-item">
-        <img src="./images/${Dino(dino).species}.png">
-        <h3>${Dino(dino).species}</h3>
-        <p>${Dino(dino).fact}</p>
+
+        const thisDino = Dino(dino);
+        thisDino.fact.push(compareHeight(human.heightInFeet, human.heightInInches, thisDino.height), compareWeight(human.weight, thisDino.weight), compareDiet(human.diet, thisDino.diet))
+
+        console.log(thisDino);
+
+
+        gridItemDinos.push(`<div class="grid-item">
+        <img src="./images/${thisDino.species}.png">
+        <h3>${thisDino.species}</h3>
+        <p>${thisDino.fact}</p>
         </div>`);
       });
+
       // Insert the Humam grid item.
-      gridItemsHtml.splice(4, 0, submit());
+      gridItemDinos.splice(4, 0, gridItemHuman);
+
       // Select the main grid div from the HTML
       const gridDiv = document.getElementById('grid');
-      gridItemsHtml.forEach((item, i) => {
+      gridItemDinos.forEach((item, i) => {
         const gridItem = document.createElement('div');
-        gridDiv.appendChild(gridItem).innerHTML = gridItemsHtml[i];
+        gridDiv.appendChild(gridItem).innerHTML = gridItemDinos[i];
       });
-      console.log(gridItemsHtml);
+      console.log(gridItemDinos);
       const formElem = document.getElementById('dino-compare');
       formElem.style.display = 'none';
     });
